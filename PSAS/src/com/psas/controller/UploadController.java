22b52f56d0a442365a5854c699fe28a95eee3951
wxl353722713 <wangxulai_0131@qq.com>
2017-02-15 14:56:54 +0800

    上传excel，王旭来。

--- PSAS/src/com/psas/controller/UploadController.java
+++ PSAS/src/com/psas/controller/UploadController.java
@@ -3,7 +3,12 @@ package com.psas.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -19,8 +24,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.psas.entity.AlarmInfo;
+import com.psas.service.AlarmInfoService;
+
 @Controller
 public class UploadController {
+	
+	@Resource(name=AlarmInfoService.BEAN_NAME)
+	AlarmInfoService alarmInfoService;
 
 	@RequestMapping("toUpload.do")
 	public ModelAndView toUpload() {
@@ -59,7 +70,41 @@ public class UploadController {
 		System.out.println(aaa);
 		System.out.println(sheet.getLastRowNum());//获得一共有多少行
 		System.out.println(sheet.getRow(0).getPhysicalNumberOfCells());//获得一行有多少元素
+		for (int i = 1;; i++) {
+			if (sheet.getRow(i).getCell(1) == null) {
+				break;
+			}
+			AlarmInfo alarmInfo = new AlarmInfo();
+			alarmInfo.setAlarmName(sheet.getRow(i).getCell(1).getStringCellValue());
+			alarmInfo.setAlarmGender(String.valueOf((int) sheet.getRow(i).getCell(2).getNumericCellValue()));
+			alarmInfo.setAlarmTelephone(sheet.getRow(i).getCell(3).getStringCellValue());
+			alarmInfo.setAlarmAddress(sheet.getRow(i).getCell(4).getStringCellValue());
+			alarmInfo.setAlarmTime(Timestamp.valueOf(sheet.getRow(i).getCell(5).getStringCellValue()));
+			alarmInfo.setAlarmFormat((int) sheet.getRow(i).getCell(6).getNumericCellValue());
+			alarmInfo.setAlarmContent(sheet.getRow(i).getCell(7).getStringCellValue());
+			alarmInfo.setAlarmCaseTime(Timestamp.valueOf(sheet.getRow(i).getCell(8).getStringCellValue()));
+			alarmInfo.setAlarmPlace(sheet.getRow(i).getCell(9).getStringCellValue());
+			alarmInfo.setReceptionPoliceman((int) sheet.getRow(i).getCell(10).getNumericCellValue());
+			alarmInfo.setHandleSituation(sheet.getRow(i).getCell(11).getStringCellValue());
+			alarmInfo.setHandlePoliceman((int) sheet.getRow(i).getCell(12).getNumericCellValue());
+			alarmInfo.setLossSiuation(sheet.getRow(i).getCell(13).getStringCellValue());
+			alarmInfo.setDeathNumber((int) sheet.getRow(i).getCell(14).getNumericCellValue());
+			alarmInfo.setInjuredNumber((int) sheet.getRow(i).getCell(15).getNumericCellValue());
+			alarmInfo.setEconomicLoss(sheet.getRow(i).getCell(16).getNumericCellValue());
+			alarmInfo.setHandleResult((int) sheet.getRow(i).getCell(17).getNumericCellValue());
+			alarmInfo.setDutyLeader((int) sheet.getRow(i).getCell(18).getNumericCellValue());
+			alarmInfoService.add(alarmInfo);
+		}
+		
 		return null;
 
+	}
+
+	public AlarmInfoService getAlarmInfoService() {
+		return alarmInfoService;
+	}
+
+	public void setAlarmInfoService(AlarmInfoService alarmInfoService) {
+		this.alarmInfoService = alarmInfoService;
 	}
 }
